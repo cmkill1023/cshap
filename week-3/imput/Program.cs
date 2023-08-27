b7a6b5c5cd5434461da067433e67b7cfb3c271a0
cmkill1023 <142684898+cmkill1023@users.noreply.github.com>
2023-08-27 18:53:33 +0700

    Add files via upload

--- week-3/imput/Program.cs
+++ week-3/imput/Program.cs
@@ -1,18 +1,39 @@
-﻿string fname="";
+﻿string firstname="";
+string lastname ="";
 double height=0.0D;
 double weight=0.0D;
+string prefix =""; 
+int ID =0;
 double BMI=0.0D;
-
-Console.Write("imput fname:");
-Console.ReadLine();
-
-Console.Write("imput heignt: ");
-height=Int32.Parse(Console.ReadLine());
-
-Console.Write("imput weight: ");
-weight=Double.Parse(Console.ReadLine());
 
 BMI=height/weight;
 
 BMI=Math.Pow(BMI,2);
-Console.WriteLine("BMI is:"+BMI);
+
+Console.Write("prefix is:");
+Console.ReadLine();
+
+Console.Write("firstname is:");
+firstname=Console.ReadLine();
+
+Console.Write("lastname is:");
+lastname =Console.ReadLine();
+
+Console.Write("ID: ");
+ID=Int32.Parse(Console.ReadLine());
+
+Console.Write("your fo heignt: ");
+height=Int32.Parse(Console.ReadLine());
+
+Console.Write("your fo weight is: ");
+weight=Double.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("*******************");
+Console.WriteLine("student id is:"+ID);
+Console.WriteLine(" fullname is:"+prefix+firstname+" "+lastname);
+Console.WriteLine("you height ");
+Console.WriteLine("you weight is:");
+Console.WriteLine("you BMI is"+BMI);
+Console.WriteLine("*******************");
