661ffd3b2956a4ad1e2657268559888400ae05df
cmkill1023 <142684898+cmkill1023@users.noreply.github.com>
2023-08-27 17:31:35 +0700

    Add files via upload

---
+++ week-3/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿/*string
+string fname = "ponsagon ";
+string lname = "gonnoi";
+string prefix = "Mr .";
+/*Console.WriteLine(fname);
+Console.WriteLine(lname);
+Console.WriteLine(prefix+fname+lname);*/
+//ตัวเลข
+/*const int myNum;
+myNum = 20;
+double NewNum=5.5D;
+ 
+:Console.WriteLine(myNum);
+:Console.WriteLine(NewNum);*/
+
+ /*const double myNum = 3.4;
+myNum = 20; // error
+Console.WriteLine (myNum);*/
+
+//console const
+/******Multiple Variables***/
+/*input1=1;
+input2=2;
+input3=3;*/
+double input1=1.0,input2=2.0,input3=3.14,input4=1.22;
+float  f1=3.33f;
+string str ="Mr";
+char ch = 'c';
+console.WriteLine(str+ch+" "+input1+input3);
---
+++ week-3/imput/Program.cs
@@ -0,0 +1,18 @@
+﻿string fname="";
+double height=0.0D;
+double weight=0.0D;
+double BMI=0.0D;
+
+Console.Write("imput fname:");
+Console.ReadLine();
+
+Console.Write("imput heignt: ");
+height=Int32.Parse(Console.ReadLine());
+
+Console.Write("imput weight: ");
+weight=Double.Parse(Console.ReadLine());
+
+BMI=height/weight;
+
+BMI=Math.Pow(BMI,2);
+Console.WriteLine("BMI is:"+BMI);
